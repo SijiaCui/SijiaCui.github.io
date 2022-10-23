ad1c577741f4d97ac19d5b583f9873e2852b7952
SijiaCui <sijiacui0908@163.com>
2022-10-23 11:27:04 +0800

    Site updated: 2022-10-23 11:27:04

--- dist/music.js
+++ dist/music.js
@@ -3,7 +3,7 @@ const ap = new APlayer({
     audio: [{
         name: 'IDK You Yet',
         artist: 'Alexander 23',
-        url: 'https://m801.music.126.net/20221021234001/b384af4e6d7fa48915cf37a7923bd9c8/jdyyaac/obj/w5rDlsOJwrLDjj7CmsOj/19743834383/a62d/02aa/7993/3c32c304f214efa7170f0c18daafb205.m4a',
+        url: 'https://m701.music.126.net/20221023114422/9e6951bbf141bb6ad06958262244817e/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/19069893523/fe6d/df95/97b5/787cbd4b837549a922847b26c41c4222.mp3',
         cover: 'http://p1.music.126.net/wBLhNIPHQUIjaqtECzdHyQ==/109951166074367835.jpg'
     }]
 });
