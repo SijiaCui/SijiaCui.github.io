3123ea4c99eb80404af46cfa6f3e6e8eb3cf4618
SijiaCui <sijiacui0908@163.com>
2022-10-24 01:18:28 +0800

    Site updated: 2022-10-24 01:18:28

--- dist/music.js
+++ dist/music.js
@@ -3,7 +3,7 @@ const ap = new APlayer({
     audio: [{
         name: 'IDK You Yet',
         artist: 'Alexander 23',
-        url: 'http://music.163.com/song/media/outer/url?id=1439043673.mp3',
+        url: 'https://sijiacui.github.io/music/IDK_You_Yet.mp3',
         cover: 'http://p1.music.126.net/wBLhNIPHQUIjaqtECzdHyQ==/109951166074367835.jpg'
     }]
 });
